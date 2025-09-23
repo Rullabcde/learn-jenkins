07dba68a91f57986bd3fd1da06d66459bd49ede6
Rullabcde <choirulrasyid09@gmail.com>
2025-09-23 11:00:47 +0700

    fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "jest";
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   moduleNameMapper: {
@@ -17,4 +16,4 @@ const config: Config = {
   coverageReporters: ["text", "lcov", "cobertura"],
 };
 
-export default config;
+module.exports = config;
