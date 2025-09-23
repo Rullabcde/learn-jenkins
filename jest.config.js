df8a3955d7d65b54ab7af4a90a350f1728738e7b
Rullabcde <choirulrasyid09@gmail.com>
2025-09-23 10:33:37 +0700

    fix test script

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,6 @@ const config = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)",
-  ],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
